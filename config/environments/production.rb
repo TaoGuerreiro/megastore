584359222a877bd8ecd3922d15a0e5313a71f040
florent guilbaud <florent.guilbaud@gmail.com>
2023-10-30 20:48:29 +0100

    postmark setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: Rails.application.credentials.host }
   config.action_mailer.delivery_method     = :postmark
   config.action_mailer.perform_caching = false
-  config.action_mailer.postmark_settings    = {
+  config.action_mailer.postmark_settings = {
     api_token: Rails.application.credentials.postmark_api_token,
     http_ssl_version: :TLSv1_2
   }
