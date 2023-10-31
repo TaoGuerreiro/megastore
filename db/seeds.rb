7a9038560fad2e0633c7f6c1a392186df1c82962
florent guilbaud <florent.guilbaud@gmail.com>
2023-10-31 18:01:07 +0100

    checkout

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,9 @@ User.destroy_all
 admin_localhost = User.create(first_name: "Ted", last_name: "Lasso", email: "admin@example.fr", password: "123456", role: "admin")
 clemence = User.create(first_name: "Clémence", last_name: "Porcheret", email: "hello@lecheveublanc.fr", password: "123456", role: "admin")
 
-if Rails.env == "development"
+unless Rails.env == "development"
   store = clemence.stores.create({
-    domain: "localhost",
+    domain: "lecheveublanc.fr",
     name: "Le Cheveu Blanc",
     slug: "lecheveublanc",
     meta_title: "Le Cheveu Blanc Illustration",
@@ -21,7 +21,7 @@ if Rails.env == "development"
   })
 else
   clemence.stores.create({
-    domain: "lecheveublanc.fr",
+    domain: "localhost",
     name: "Le Cheveu Blanc",
     slug: "lecheveublanc",
     meta_title: "Le Cheveu Blanc Illustration",
@@ -30,32 +30,32 @@ else
     instagram_url: "https://www.instagram.com/le_cheveu_blanc/",
     facebook_url: "https://www.facebook.com/lecheveublanc/"
   })
-end
-categories = []
-[:stickers, :print, :illustration].each do |category|
-  category = Category.create({
-    store: store,
-    name: category
-  })
+  categories = []
+  [:stickers, :print, :illustration].each do |category|
+    category = Category.create({
+      store: store,
+      name: category
+    })
 
-  categories << category
-end
+    categories << category
+  end
 
-10.times do
-  file = URI.open("https://source.unsplash.com/random/300x300/?illustration")
-  item = Item.new({
-    name: Faker::Commerce.product_name,
-    description:  Faker::Commerce.material + " " + Faker::Commerce.product_name,
-    price_cents: Faker::Number.number(digits: 5),
-    price_currency: "EUR",
-    store: store,
-    category: categories.sample,
-    stock: Faker::Number.between(from: 0, to: 100),
-    weight: Faker::Number.decimal(l_digits: 2, r_digits: 2),
-    length: Faker::Number.between(from: 1, to: 100),
-    width: Faker::Number.between(from: 1, to: 100),
-    height: Faker::Number.between(from: 1, to: 100)
-  })
-  item.photos.attach(io: file, filename: "nes.png", content_type: "image/png")
-  item.save
+    10.times do
+      file = URI.open("https://source.unsplash.com/random/300x300/?illustration")
+      item = Item.new({
+        name: Faker::Commerce.product_name,
+        description:  Faker::Commerce.material + " " + Faker::Commerce.product_name,
+        price_cents: Faker::Number.number(digits: 5),
+        price_currency: "EUR",
+        store: store,
+        category: categories.sample,
+        stock: Faker::Number.between(from: 0, to: 100),
+        weight: Faker::Number.decimal(l_digits: 2, r_digits: 2),
+        length: Faker::Number.between(from: 1, to: 100),
+        width: Faker::Number.between(from: 1, to: 100),
+        height: Faker::Number.between(from: 1, to: 100)
+      })
+      item.photos.attach(io: file, filename: "nes.png", content_type: "image/png")
+      item.save
+    end
 end
