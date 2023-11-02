22bfb3ca350349b76de05456c8d6602ccc7cbba5
florent guilbaud <florent.guilbaud@gmail.com>
2023-11-02 13:50:46 +0100

    turbo on cart

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ApplicationRecord
   belongs_to :category
 
 
-  has_many :order_items
+  has_many :order_items, dependent: :destroy
   has_many :orders, through: :order_items
 
   has_many_attached :photos
