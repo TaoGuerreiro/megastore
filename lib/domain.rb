f0547541865de382d9a8908c4f6f7feade543584
florent guilbaud <florent.guilbaud@gmail.com>
2023-11-17 18:47:11 +0100

    theme unafe

--- lib/domain.rb
+++ lib/domain.rb
@@ -1,8 +1,5 @@
 class Domain
   def self.matches? request
-    puts "*" * 200
-    puts request.domain
-    puts "*" * 200
     request.domain == 'localhost' || request.domain == 'lecheveublanc.fr' || request.domain == 'ngrok.io' || request.domain == 'unsafehxc.fr'
   end
 end
