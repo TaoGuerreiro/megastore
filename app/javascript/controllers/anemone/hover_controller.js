c43f73475436f57b26551907dd01cb5bc14b9c64
florent guilbaud <florent.guilbaud@gmail.com>
2023-11-23 17:56:55 +0100

    fixing tailwind

--- app/javascript/controllers/anemone/hover_controller.js
+++ app/javascript/controllers/anemone/hover_controller.js
@@ -5,12 +5,12 @@ export default class extends Controller {
   static targets = ["button"]
 
   scale(event) {
-    event.currentTarget.classList.remove("w-1/6", "grow");
+    event.currentTarget.classList.remove("w-1/6", "grow", "grayscale");
     event.currentTarget.classList.add("w-1/2");
   }
 
   unscale(event) {
-    event.currentTarget.classList.add("w-1/6", "grow");
+    event.currentTarget.classList.add("w-1/6", "grow", "grayscale");
     event.currentTarget.classList.remove("w-1/2");
   }
 }
