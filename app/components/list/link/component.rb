2d78277ad5947d26fff57c0cf5e0f6952fcc90f0
florent guilbaud <florent.guilbaud@gmail.com>
2024-04-01 12:35:56 +0200

    fix: fix list

--- app/components/list/link/component.rb
+++ app/components/list/link/component.rb
@@ -1,12 +1,18 @@
 module List
   module Link
     class Component < ViewComponent::Base
-      attr_accessor :icon, :label, :path
+      attr_accessor :icon, :label, :path, :options
 
-      def initialize(icon: nil, label:, path:)
+      def initialize(icon: nil, label:, path:, options: {})
         @icon = icon
         @label = label
         @path = path
+        @options = options
+      end
+
+      def options_with_classes
+        @options[:class] = "flex items-center p-2 text-sm font-semibold leading-6 rounded-md text-contrast hover:text-primary hover:bg-light group gap-x-3"
+        @options
       end
     end
   end
