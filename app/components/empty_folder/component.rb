596820a503c740a3d156a53709f87996fcdcc7c5
florent guilbaud <florent.guilbaud@gmail.com>
2024-04-05 13:35:34 +0200

    feat: adding empry state component

---
+++ app/components/empty_folder/component.rb
@@ -0,0 +1,9 @@
+module EmptyFolder
+  class Component < ViewComponent::Base
+    attr_reader :title, :subtitle
+    def initialize(title:, subtitle:)
+      @title = title
+      @subtitle = subtitle
+    end
+  end
+end
