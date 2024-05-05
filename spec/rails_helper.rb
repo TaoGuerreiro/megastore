4a66c665f484d417951d86488e3a0c598b474bd2
florent guilbaud <florent.guilbaud@gmail.com>
2024-05-05 21:45:40 +0200

    test: CI

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
+
+require 'spec_helper'
 require_relative '../config/environment'
 Rails.application.eager_load!
 
