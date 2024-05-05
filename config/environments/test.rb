fee8ea11949eb6c797281075138df3b83c73c25e
florent guilbaud <florent.guilbaud@gmail.com>
2024-05-06 00:00:17 +0200

    test: CI

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -18,6 +18,9 @@ Rails.application.configure do
   # system, or in some way before deploying your code.
   config.eager_load = ENV['CI'].present?
 
+  config.require_master_key = true
+
+
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
