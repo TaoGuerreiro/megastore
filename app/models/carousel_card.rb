785d45cea4c5cc8109ace78809159351fdc9a9c7
florent guilbaud <florent.guilbaud@gmail.com>
2024-11-12 15:42:50 +0100

    fix

--- app/models/carousel_card.rb
+++ app/models/carousel_card.rb
@@ -15,7 +15,7 @@ class CarouselCard < ApplicationRecord
   end
 
   def update_position
-    max_y = CarouselCard.all.min_by(&:position_y).position_y
-    card.update!(position_x: 3, position_y: max_y - 1)
+    min_y = CarouselCard.all.min_by(&:position_y).position_y
+    card.update!(position_x: 3, position_y: min_y)
   end
 end
