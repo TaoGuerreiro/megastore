9d90d74222913677843cf7b73480b85796b64365
florent guilbaud <florent.guilbaud@gmail.com>
2025-02-15 20:15:12 +0100

    feat: adding logo

--- app/javascript/controllers/ttt/scroll_down_controller.js
+++ app/javascript/controllers/ttt/scroll_down_controller.js
@@ -2,15 +2,30 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
 
-  connect() {
-    console.log("coucou");
-  }
-
   scroll() {
     window.scrollBy({
       top: window.innerHeight,
       left: 0,
       behavior: 'smooth'
     });
+  }
+
+  connect() {
+    window.addEventListener("scroll", this.toggleVisibility.bind(this));
+  }
+
+  disconnect() {
+    window.removeEventListener("scroll", this.toggleVisibility.bind(this));
+  }
+
+  toggleVisibility() {
+    const scrollPosition = window.scrollY;
+    const windowHeight = window.innerHeight;
+
+    if (scrollPosition > windowHeight) {
+      this.element.classList.add("visible");
+    } else {
+      this.element.classList.remove("visible");
+    }
   }
 }
